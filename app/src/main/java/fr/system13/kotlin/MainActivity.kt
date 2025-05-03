a95e4e99c4db1fcf0ce3376ce463ffd610146119
Ferdinand <ferdi.croquel@wanadoo.fr>
2025-05-03 22:22:02 +0200

    Remplacement des strings en dur par des ressources

--- app/src/main/java/fr/system13/kotlin/MainActivity.kt
+++ app/src/main/java/fr/system13/kotlin/MainActivity.kt
@@ -4,57 +4,22 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.focus.FocusManager
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import fr.system13.kotlin.ui.theme.KotlinLessonTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,7 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KotlinLessonTheme {
-                ImcScaffold()
+                CaloriesScaffold()
             }
         }
     }
@@ -71,28 +36,43 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun DefaultPreview() {
         KotlinLessonTheme {
-            ImcScaffold()
+            CaloriesScaffold()
         }
     }
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @Composable
-    fun ImcScaffold() {
+    fun CaloriesScaffold() {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Scaffold()
+            Scaffold(
+                topBar = { CaloriesAppBar() },
+            )
             { contentPadding ->
                 Column(modifier = Modifier.padding(contentPadding)) {
-                    ImcBody()
+                    CaloriesBody()
                 }
             }
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun ImcBody() {
+    fun CaloriesAppBar() {
+        TopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.exo_06_calories_appbar),
+                    style = MaterialTheme.typography.headlineSmall,
+                )
+            }
+        )
+    }
+
+    @Composable
+    fun CaloriesBody() {
         val focusManager = LocalFocusManager.current
         Surface(modifier = Modifier.fillMaxSize(), onClick = { focusManager.clearFocus() })
         {
